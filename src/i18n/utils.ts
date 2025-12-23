f12a12f42ced4fa5bfba8193f487a924c6f16dfe
nguoimegame <quang49a9@gmail.com>
2025-12-24 01:44:30 +0700

    clean code

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -3,12 +3,12 @@ import {defaultLang, ui} from './ui';
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
   const cleanLang = lang?.replace(/\.html$/, '');
-  if (cleanLang in ui) return cleanLang as keyof typeof ui;
+  if (cleanLang in ui) {return cleanLang as keyof typeof ui;}
   return defaultLang;
 }
 
 export function useTranslations(lang: keyof typeof ui) {
-  return function t(key: keyof typeof ui[typeof defaultLang], ...args: any[]) {
+  return function t(key: keyof typeof ui[typeof defaultLang], ...args: string[]) {
     const langTranslations = ui[lang] as Record<string, string>;
     let translation = langTranslations[key as string] || ui[defaultLang][key];
     if (args.length > 0) {
